c2094d364e02798c99e24ceaac3f3e708e49641e
Yilei Yao <jzyao@users.noreply.github.com>
2019-07-31 15:54:29 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,17 +12,18 @@ import (
 func recordMetrics() {
         go func() {
                 for {
-                        opsProcessed.Inc()
+                        myGague.Add(11)
                         time.Sleep(2 * time.Second)
                 }
         }()
 }
 
 var (
-        opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-                Name: "myapp_processed_ops_total",
-                Help: "The total number of processed events",
-        })
+         myGague = promauto.NewGauge(prometheus.GaugeOpts{
+         Name: "my_example_gauge_data",
+         Help: "my example gauge data",
+         ConstLabels:map[string]string{"error":""},
+         })
 )
 
 func main() {
